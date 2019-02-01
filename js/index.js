be274ad476b933f6e3a53da925bd0d4b0b54a0af
Rane Wallin <RAN2143066@maricopa.edu>
2019-02-01 15:18:57 -0700

    event #2 and #3, text enlarges and returns to normal as mouse enters and leaves

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 const logo = document.querySelector('.logo-heading');
 const page = document.querySelector('body');
 const navLinks = Array.from(document.querySelector('nav').children);
+const paragraphs = Array.from(document.querySelectorAll('p'));
 
 // #1, change the color of the logo as the page scrolls
 window.addEventListener('scroll', e => {
@@ -12,8 +13,20 @@ window.addEventListener('scroll', e => {
     logo.style.color = `rgb(${colorCode}, 0, 0)`;
 });
 
+// #2,  enlarge text when mouseover
+ paragraphs.forEach(paragraph => paragraph.addEventListener('mouseover', e => {
+     e.target.style.fontSize = "2.4rem";
+ }));
+
+ // #3, reduce text when mouse out
+paragraphs.forEach(paragraph => paragraph.addEventListener('mouseleave', e => {
+     e.target.style.fontSize = "1.6rem";
+}));
+
+
+
 // prevent page from refreshing when clicking links
-navLinks.forEach(link => addEventListener('click', e => {
+navLinks.forEach(link => link.addEventListener('click', e => {
     e.preventDefault();
     e.stopPropagation();
 }));
